f5c1e594c2586add5d31ef9d0929efc9180a6fb5
XxlgmbxX <luizbiel222@gmail.com>
2024-09-01 23:17:46 -0300

    primeiro commit pato putao

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const express = require("express")
+const path = require('path')
+const { WebSocketServer } = require("ws")
+require("dotenv").config()
+
+//
+
+const server = express()
+const port = process.env.PORT || 3000
+
+//
+
+// Inicia o servidor HTTP
+const serverExpress = server.listen(port, () => {
+    console.log("Server is running")
+})
+
+//Arquivo html
+server.get("/", (_req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+//
+
+// pagina de erro
+server.use((_req, res) => {
+    res.status(404).send("Página não encontrada")
+})
+
+//
+
+const wss = new WebSocketServer({ server: serverExpress })
+
+wss.on("connection", (ws) => {
+    ws.on("error", console.error)
+
+    ws.on("message", (data) => {
+        //console.log(data.toString())
+        wss.clients.forEach((client) => client.send(data.toString()))
+    })
+    console.log("client connected")
+})

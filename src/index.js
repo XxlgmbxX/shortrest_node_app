330ef2d9a441e7163b0e4b5b58f99bdcaad57738
XxlgmbxX <luizbiel222@gmail.com>
2024-09-03 02:36:06 -0300

    asdf

--- src/index.js
+++ src/index.js
@@ -1,42 +1,37 @@
-const express = require("express")
-const path = require('path')
-const { WebSocketServer } = require("ws")
-require("dotenv").config()
-const router = express.Router();
-//
+const express = require("express");
+const path = require('path');
+const { WebSocketServer } = require("ws");
+require("dotenv").config();
 
-const server = express()
-const port = process.env.PORT || 3000
+const server = express();
+const port = process.env.PORT || 3000;
+
+// Servir arquivos estáticos da pasta 'src'
 server.use(express.static(path.join(__dirname, 'src')));
-//
+
+// Enviar o arquivo HTML na rota raiz
+server.get("/", (_req, res) => {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
+
+// Página de erro 404
+server.use((_req, res) => {
+    res.status(404).send("Página não encontrada");
+});
 
 // Inicia o servidor HTTP
 const serverExpress = server.listen(port, () => {
-    console.log("Server is running")
-})
+    console.log(`Server is running on port ${port}`);
+});
 
-//Arquivo html
-router.get("/", (_req, res) => {
-    res.sendFile(path.join(__dirname + 'index.html'))
-})
-
-//
-
-// pagina de erro
-server.use((_req, res) => {
-    res.status(404).send("Página não encontrada")
-})
-
-//
-
-const wss = new WebSocketServer({ server: serverExpress })
+// Configuração do WebSocket Server
+const wss = new WebSocketServer({ server: serverExpress });
 
 wss.on("connection", (ws) => {
-    ws.on("error", console.error)
+    ws.on("error", console.error);
 
     ws.on("message", (data) => {
-        //console.log(data.toString())
-        wss.clients.forEach((client) => client.send(data.toString()))
-    })
-    console.log("client connected")
-})
+        wss.clients.forEach((client) => client.send(data.toString()));
+    });
+    console.log("client connected");
+});
